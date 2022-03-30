a39fca4527f9da8b3b1f2aa6bc80239724e0d66d
alanzote <arbunese@gmail.com>
2022-03-30 09:59:47 -0300

    Finished Trabalho 03

--- IA/Program.cs
+++ IA/Program.cs
@@ -29,15 +29,15 @@ GN.AddNode(N7);
 
 // Conectamos todos os Nós, passando o Custo de cada caminho.
 GN.ConnectNode(N0, N1, 1);
-GN.ConnectNode(N1, N3, 1);
-GN.ConnectNode(N3, N2, 10);
-GN.ConnectNode(N2, N1, 1);
-GN.ConnectNode(N2, N4, 1);
-GN.ConnectNode(N3, N5, 1);
-GN.ConnectNode(N5, N6, 1);
+GN.ConnectNode(N1, N3, 3);
+GN.ConnectNode(N3, N2, 5);
+GN.ConnectNode(N2, N1, 2);
+GN.ConnectNode(N2, N4, 6);
+GN.ConnectNode(N3, N5, 4);
+GN.ConnectNode(N5, N6, 2);
 GN.ConnectNode(N6, N2, 1);
-GN.ConnectNode(N5, N4, 1);
-GN.ConnectNode(N4, N7, 1);
+GN.ConnectNode(N5, N4, 3);
+GN.ConnectNode(N4, N7, 5);
 
 // Primeiro, vamos demonstrar para o Usuário a disposição dos nossos nós.
 // Para utilizar esse Display, de uma olhada no Readme.md do GraphUtils
@@ -47,6 +47,7 @@ GN.Display();
 
 /// Assim que fechamos o display, está na hora de resolver as questões práticas.
 /// Sabemos que o Estado Final é o Carrinho estar presente em N0.
+Node NT = N0;
 
 // Para isso, precisamos pedir os estado inicial para o usuário.
 Console.Write("Digite o Nome do Nó que você Quer como Estado Inicial: ");
@@ -58,7 +59,7 @@ if (NI == null)
 
 // Executamos a Busca em Largura, saindo de NI indo para N0. (Questão 5)
 // Essa função eu já tinha implementado para Resolução de Problemas com Grafos.
-GN.BreadthSearch(NI, N0, out List<Node> Path);
+GN.BreadthSearch(NI, NT, out List<Node> Path);
 
 // Demonstrar o nosso caminho.
 GraphExtensions.PrintArrayOfNodes(Path, "Caminho de Busca em Largura Encontrado para N0");
@@ -68,7 +69,7 @@ Path.Clear();
 
 // Executamos a Busca em Profundidade, saindo de NI indo para N0. (Questão 6)
 // Essa função eu já tinha implementado para Resolução de Problemas com Grafos.
-GN.DepthSearch(NI, N0, out Path);
+GN.DepthSearch(NI, NT, out Path);
 
 // Demonstrar o nosso caminho.
 GraphExtensions.PrintArrayOfNodes(Path, "Caminho de Busca em Profundidade Encontrado para N0");
@@ -76,13 +77,14 @@ GraphExtensions.PrintArrayOfNodes(Path, "Caminho de Busca em Profundidade Encont
 // Limpar o Path para a Próxima Busca.
 Path.Clear();
 
-// Pedir o Limite para nossa Busca em Profundidade Limitada.
+// Pedir o Limite para nossa a Busca em Profundidade Limitada.
+Console.WriteLine();
+Console.Write("Digite a Profundidade Máxima para a Próxima Busca em Profundidade: ");
 int Limit = int.Parse(Console.ReadLine()?.Trim()!);
 
 // Executamos a Busca em Profundidade Limitada, saindo de NI indo para N0. (Questão 7)
 // Essa função eu adicionei um novo parametro na busca em profundidade para considerar um limite.
-// TODO: Create Parameter.
-GN.DepthSearch(NI, N0, out Path);
+GN.DepthSearch(NI, NT, out Path, Limit);
 
 // Demostrar o nosso caminho.
 GraphExtensions.PrintArrayOfNodes(Path, "Caminho de Busca em Profundidade Limitada Encontrado para N0");
@@ -90,9 +92,14 @@ GraphExtensions.PrintArrayOfNodes(Path, "Caminho de Busca em Profundidade Limita
 // Limpar o Path para a Próxima Busca.
 Path.Clear();
 
+// Pedir o Limite para a Busca de Aprofundamento Iterativo.
+Console.WriteLine();
+Console.Write("Digite a Profundidade Máxima para a Busca de Aprofundamento Iterativo: ");
+Limit = int.Parse(Console.ReadLine()?.Trim()!);
+
 // Executamos a Busca em Aprofundamento Iterativo, saindo de NI indo para N0. (Questão 8)
 // Essa função é nova e foi implementada para esse TDE.
-// TODO: Create Function.
+GN.IterativeDeepening(NI, NT, out Path, Limit);
 
 // Demonstrar o nosso caminho.
 GraphExtensions.PrintArrayOfNodes(Path, "Caminho de Busca em Aprofundamento Iterativo Encontrado para N0");
